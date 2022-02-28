f271d2f59c6b0737aaf4cb515e363b7198b6f0cd
Itay <itay.nextweb@gmail.com>
2022-02-28 13:06:02 +0200

    Tuples

--- Tuple/tuple.py
+++ Tuple/tuple.py
@@ -4,24 +4,43 @@ print("\n-----------------------------------------\n")
 # *** You cannot change the values inside a tuple once you have created it!
 
 my_tuple = ("Apple", "Banana", "Cherry")
-# my_tuple[0] = "Mango" # ERROR!
 
-print(f"print(my_tuple) -> {my_tuple}")
-print("\n-----------------------------------------\n")
+# my_tuple[0] = "Mango"     # ERROR!
 
-# Tuple items are indexed, the first item has index [0], the second item has index [1] and so on...
-# To access the tuple items use [] and specify the index you want to access:
-
-print(f"Access item at index 1: my_tuple[1] = {my_tuple[1]}")
-print(f"Access item at index -1(Last): my_tuple[-1] = {my_tuple[-1]}")
+print(my_tuple[0])
 
 
-print("\n-----------------------------------------\n")
 
-# Allow duplicate values:
-my_tuple = ("apple", "banana", "cherry", "apple", "cherry")
-print(f"my_tuple with duplicate values:\t {my_tuple}")
 
-# Tuple is `iterable`, you can iterate over it's items:
-for x in my_tuple:
-    print(f"x = {x}")
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# print(f"print(my_tuple) -> {my_tuple}")
+# print("\n-----------------------------------------\n")
+#
+# # Tuple items are indexed, the first item has index [0], the second item has index [1] and so on...
+# # To access the tuple items use [] and specify the index you want to access:
+#
+# print(f"Access item at index 1: my_tuple[1] = {my_tuple[1]}")
+# print(f"Access item at index -1(Last): my_tuple[-1] = {my_tuple[-1]}")
+#
+#
+# print("\n-----------------------------------------\n")
+#
+# # Allow duplicate values:
+# my_tuple = ("apple", "banana", "cherry", "apple", "cherry")
+# print(f"my_tuple with duplicate values:\t {my_tuple}")
+#
+# # Tuple is `iterable`, you can iterate over it's items:
+# for x in my_tuple:
+#     print(f"x = {x}")
