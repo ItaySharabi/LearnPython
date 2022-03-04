312ec1e861468436b75073bc5a1b4cf6ae249cfc
Itay <itay.nextweb@gmail.com>
2022-03-04 14:19:04 +0200

    window example

--- GUI/tkinter/02_window_example.py
+++ GUI/tkinter/02_window_example.py
@@ -8,7 +8,9 @@ frm = ttk.Frame(root, padding=10)
 frm.grid(rows=3, columns=3)
 
 ttk.Label(master=frm, text='Enter your name: ').grid(row=0, column=0)
+
 ttk.Label(frm, text="Hello World!").grid(row=1, column=1)
+
 ttk.Button(frm, text="Quit", command=root.destroy).grid(row=2, column=2)
 
 root.mainloop()
