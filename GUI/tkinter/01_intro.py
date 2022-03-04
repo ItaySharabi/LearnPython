fbe166f5b4e151348fc8e7e57378c36a1e2160bb
Itay <itay.nextweb@gmail.com>
2022-03-04 14:17:36 +0200

    minor update

--- GUI/tkinter/01_intro.py
+++ GUI/tkinter/01_intro.py
@@ -6,9 +6,10 @@ from tkinter import ttk  # Widget's library
 root = Tk()
 
 # Create a frame widget
+# frame = ttk.Frame() # Initialize a Frame object with no master!
 frame = ttk.Frame(master=root, padding=10)
 
-# What configurations does has a `Frame` Widget?
+# What configurations does a `Frame` Widget have?
 print(frame.configure().keys())
 frame.configure(height=400, width=400)  # הגדרת פרמטרים של "חלון"
 frame.pack()       # נשים לב ששום שינוי לא נעשה לפני שנקרא ל
